2d0a24204c68bbbff0a18e1addf697296b26087a
BinhThanh313 <binhdt1303@gmail.com>
2026-05-08 17:14:20 +0700

    auth login admin fe

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,14 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // <--- 1. BẠN CẦN THÊM DÒNG NÀY Ở ĐÂY
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable; // <--- 2. VÀ THÊM Chữ HasApiTokens VÀO DÒNG NÀY
 
     /**
      * Get the attributes that should be cast.
